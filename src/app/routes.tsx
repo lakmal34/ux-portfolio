2908ffefe93c31a3c8241bb6d761e233faee58c0
lakmalkaviratne <lakmalkaviratne@gmail.com>
2026-08-04 19:06:17 +1000

    Add interactive Career timeline page with Framer Motion

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import { Home } from "./pages/Home";
 import { CaseStudyDetail } from "./pages/CaseStudyDetail";
 import { About } from "./pages/About";
+import { Career } from "./pages/Career";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
   {
     path: "/about",
     Component: About,
+  },
+  {
+    path: "/career",
+    Component: Career,
   },
   {
     path: "/case-study/:id",
